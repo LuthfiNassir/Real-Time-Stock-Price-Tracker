c05c57e70ea76769a4f79077d31dfcc3adb82684
LUTHFI NASSIR <120134408+LuthfiNassir@users.noreply.github.com>
2024-08-21 11:50:53 +0530

    Create tracker.py

---
+++ tracker.py
@@ -0,0 +1,34 @@
+import pandas as pd  
+import datetime  # Importing datetime for handling date and time (not used in this script)
+import requests  # Importing requests to make HTTP requests
+from requests.exceptions import ConnectionError  # Importing ConnectionError to handle request exceptions
+from bs4 import BeautifulSoup  # Importing BeautifulSoup to parse HTML content
+
+# Function to extract specific text data from the HTML content
+def webContent_div(webContent, classPath):
+    webContent_div = webContent.find_all('div', {'class': classPath})  # Find all div elements with the specified class
+    try:
+        spans = webContent_div[0].find_all('span')  # Find all span elements within the first div
+        texts = [span.get_text() for span in spans]  # Extract the text from each span element
+    except IndexError:  # Handle case where no div or span is found
+        texts = []
+    return texts
+
+# Function to get real-time stock price and change for a given stock code
+def real_time(stockCode):
+    url = 'https://finance.yahoo.com/quote/' + stockCode  # Construct the URL for the stock
+    try:
+        r = requests.get(url)  # Send a GET request to the URL
+        webContent = BeautifulSoup(r.text, 'lxml')  
+        texts = webContent_div(webContent, 'bottom svelte-ezk9pj')  # Extract price and change from the parsed content
+        if texts != []:
+            price, change = texts[0], texts[1]  # Assign price and change if found
+        else:
+            price, change = [], []  
+    except ConnectionError: 
+        price, change = [], []
+    return price, change
+
+# Prompt the user to enter a stock code and print the real-time data
+Stock = input("Enter the stock: ")
+print(real_time(Stock))
